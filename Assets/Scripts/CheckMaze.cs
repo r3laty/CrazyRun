751c41e2effc671c6fd3b164f30232a47a0e23b8
r3laty <lolkers.ilya@gmail.com>
2024-01-10 14:18:38 +0300

    Make animate the passage over maze

---
+++ Assets/Scripts/CheckMaze.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.UI;
+
+public class CheckMaze : MonoBehaviour
+{
+    [SerializeField] private Button checkButton;
+    [SerializeField] private PlayableDirector playableDirector;
+    private void Start()
+    {
+        checkButton.onClick.AddListener(AnimateThePassage);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            checkButton.gameObject.SetActive(true);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            checkButton.gameObject.SetActive(false);
+        }
+    }
+    private void AnimateThePassage()
+    {
+        playableDirector.Play();
+    }
+}
